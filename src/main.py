d7e74f60969814a9e906bd98d85f9b66b3955676
Lucas Romano <lucasromanoidalgo@gmail.com>
2025-05-24 00:41:54 -0300

    Aula 4: limpeza de dados, preenchimento de nulos, remoção de duplicatas

--- src/main.py
+++ src/main.py
@@ -28,6 +28,15 @@ def main():
     df.sort_values(by='nota', ascending=False).to_csv("data/processed/alunos_ordenados.csv", index=False)
 
     print("Arquivo 'alunos_ordenados.csv' salvo em data/processed/")
+
+    print("\n valores ausentes por coluna:")
+    print(df.isnull().sum())
+
+    df['nota'] = df['nota'].fillna(0) # Preenche notas nulas com 0
+    df.drop_duplicates(inplace=True) # Remover duplicadas
+    df.rename(columns={"nome": "aluno"}, inplace=True) # Renomear colunas
+    df['idade'] = df['idade'].astype(int) # Alterar tipo de dados (ex: string --> int)
+    df.to_csv("data/processed/alunos_limpos.csv", index=False) # Salvar dados limpos
     
 if __name__ == "__main__":
     main()
